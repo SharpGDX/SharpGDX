e8eb4b1808b965bd8b989daf9b6477bd067d1411
Richard Preziosi <17318983+RichardPreziosi@users.noreply.github.com>
2024-10-16 01:02:06 -0400

    Enabling NewInstance in ClassReflection.cs.

--- src/SharpGDX/Utils/Reflect/ClassReflection.cs
+++ src/SharpGDX/Utils/Reflect/ClassReflection.cs
@@ -80,18 +80,35 @@ namespace SharpGDX.Utils.Reflect
 		//		return Modifier.isAbstract(c.getModifiers());
 		//	}
 
-		//	/** Creates a new instance of the class represented by the supplied Class. */
-		//	static public <T> T newInstance (Class<T> c) throws ReflectionException {
-		//		try {
-		//			return c.newInstance();
-		//		} catch (InstantiationException e) {
-		//			throw new ReflectionException("Could not instantiate instance of class: " + c.getName(), e);
-		//		} catch (IllegalAccessException e) {
-		//			throw new ReflectionException("Could not instantiate instance of class: " + c.getName(), e);
-		//		}
-		//	}
+        /** Creates a new instance of the class represented by the supplied Class. */
+		// TODO: Does this really need 'Type c'? -RP
+		// TODO: Can it just use the generic type and call Activator.CreateInstance<T>()? -RP
+        static public T newInstance<T>(Type c) //throws ReflectionException
+        {
+            try
+            {
+                return (T)Activator.CreateInstance(c);
+            }
+            catch (Exception e)
+            {
+                throw new ReflectionException("Could not instantiate instance of class: " + c.Name, e);
+            }
+			// TODO: Need to properly mimic. -RP
+            //try
+            //{
+            //    return c.newInstance();
+            //}
+            //catch (InstantiationException e)
+            //{
+            //    throw new ReflectionException("Could not instantiate instance of class: " + c.getName(), e);
+            //}
+            //catch (IllegalAccessException e)
+            //{
+            //    throw new ReflectionException("Could not instantiate instance of class: " + c.getName(), e);
+            //}
+        }
 
-		//	/** Returns the Class representing the component type of an array. If this class does not represent an array class this method
+        //	/** Returns the Class representing the component type of an array. If this class does not represent an array class this method
 		//	 * returns null. */
 		//	static public Class getComponentType (Class c) {
 		//		return c.getComponentType();
